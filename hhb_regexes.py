73d6447da6890cbad6b5fda1c1796fa05c78fe06
Sam Craven <sam@seemsgood.com>
2020-10-15 18:30:58 -0500

    Improved chron.com matching
    Had to temporarily disable the bot as bad matches on this were too
    great :/

--- hhb_regexes.py
+++ hhb_regexes.py
@@ -3,7 +3,7 @@ regexes = {
 	"www.khou.com":"(?<=\"article__headline\"\>)(.*?)(?=<\/h1>)",
 	"www.houstonpublicmedia.org":"(?<=\>)(.*?)(?=<\/h1>)",
 	"www.click2houston.com":"(?<=\"headline\":\")(.*?)(?=\",\"description\")",
-	"www.chron.com":"(?<=og:title\" content=\")(.*)(?=\"\ +\/>)",
+	"www.chron.com":"(?<=og:title\" content=\")(.*?)(?=\"\ ?\/>)",
 	"chron slideshow":"(?<=og:title\" content=\")(.*)(?=\"\/><meta property=\"og:description)",
 	"abc13.com":"(?<=class=\"headline\">)(.*)(?=<\/h1>)",
 	"spacecityweather.com":"(?<=<h1 class=\"amp-wp-title\">)(.*?)(?=</h1>)"
